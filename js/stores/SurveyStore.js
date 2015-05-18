728f2b5df97ccd8aa3c1b16509f6580b2d76d56c
captainill <jonthomas0@gmail.com>
2015-05-17 19:44:01 -0700

    adding success handling to store

--- js/stores/SurveyStore.js
+++ js/stores/SurveyStore.js
@@ -1,25 +1,48 @@
 import AppDispatcher from '../dispatcher/AppDispatcher';
 import { createStore, mergeIntoBag, isInBag } from '../utils/StoreUtils';
-import selectn from 'selectn';
+//import selectn from 'selectn';
+import AppConstants from '../constants/AppConstants';
 
-const _surveys = {};
+let _currentID = null;
+let _surveys = {};
 
 const SurveyStore = createStore({
-  contains(id) {
-    return isInBag(_surveys, id);
-  },
-
+  
   get(id) {
     return _surveys[id];
+  },
+
+  getCurrentID: function() {
+    return _currentID;
+  },
+
+  getCurrent: function() {
+    return this.get(this.getCurrentID());
+  },  
+
+  contains(id) {
+    return isInBag(_surveys, id);
   }
 });
 
 SurveyStore.dispatchToken = AppDispatcher.register(action => {
-  const responseSurveys = selectn('response.surveys', action);
-  if (responseSurveys) {
-    mergeIntoBag(_surveys, responseSurveys);
-    SurveyStore.emitChange();
+
+  switch(action.type) {
+
+    case AppConstants.REQUEST_SURVEY_SUCCESS:
+      let survey = action.response;
+      if (survey) {
+        _currentID = survey.id;
+        mergeIntoBag(_surveys, survey);
+        SurveyStore.emitChange();
+      }
+    break;
+
+    default:
+      // do nothing
   }
+
 });
+
 
 export default SurveyStore;
