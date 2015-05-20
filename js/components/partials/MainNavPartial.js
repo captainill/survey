2ef22a279df707a0d8e11ebc88a5c0511f954997
captainill <jonthomas0@gmail.com>
2015-05-19 20:17:32 -0700

    cleanup

--- js/components/partials/MainNavPartial.js
+++ js/components/partials/MainNavPartial.js
@@ -6,8 +6,8 @@ export default class MainNavPartial extends React.Component {
     return (
       <ul className="">
       	<li><Link to='home' >Home</Link></li>
-        <li className={(this.props.surveyID === 's_1') ? 'active' : ''}><Link to='survey' params={{id: "s_1"}}>Survey 1</Link></li>
-        <li className={(this.props.surveyID === 's_2') ? 'active' : ''}><Link to='survey' params={{id: "s_2"}}>Survey 2</Link></li>
+        <li className={(this.props.surveyID === 's_1') ? 'active' : ''}><Link to='survey' params={{id: "s_1"}}>Morning</Link></li>
+        <li className={(this.props.surveyID === 's_2') ? 'active' : ''}><Link to='survey' params={{id: "s_2"}}>Afternoon</Link></li>
       </ul>
     );
   }
